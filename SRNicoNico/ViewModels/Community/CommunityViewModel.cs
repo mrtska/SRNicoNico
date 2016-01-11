1fd6eed137d3d39ccc9e831e307a15d88fd596d6
mrtska <mrtska.starring@gmail.com>
2016-01-12 01:44:04 +0900

    コミュニティフロントエンド

--- SRNicoNico/ViewModels/Community/CommunityViewModel.cs
+++ SRNicoNico/ViewModels/Community/CommunityViewModel.cs
@@ -12,21 +12,62 @@ using Livet.EventListeners;
 using Livet.Messaging.Windows;
 
 using SRNicoNico.Models.NicoNicoWrapper;
+using System.Threading.Tasks;
 
 namespace SRNicoNico.ViewModels {
     public class CommunityViewModel : TabItemViewModel {
 
         private readonly NicoNicoCommunity Community;
 
-        public CommunityViewModel(string url) {
+
+        #region Content変更通知プロパティ
+        private NicoNicoCommunityContent _Content;
+
+        public NicoNicoCommunityContent Content {
+            get { return _Content; }
+            set { 
+                if(_Content == value)
+                    return;
+                _Content = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        public CommunityViewModel(string url) : base("読込中") {
 
             Community = new NicoNicoCommunity(url);
 
-            Community.GetCommunity();
 
             App.ViewModelRoot.AddTabAndSetCurrent(this);
+
+            IsActive = true;
+            Content = Community.GetCommunity();
+            IsActive = false;
+            Name = Content.CommunityTitle;
         }
 
+
+        public void OpenBrowser() {
+
+            System.Diagnostics.Process.Start(Content.CommunityUrl);
+        }
+
+        public void Close() {
+
+            App.ViewModelRoot.RemoveTabAndLastSet(this);
+        }
+
+        public void Reflesh() {
+
+            Task.Run(() => {
+
+                Close();
+                new CommunityViewModel(Content.CommunityUrl);
+            });
+
+        }
 
 
 
