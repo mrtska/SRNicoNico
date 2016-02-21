99192ce7f4bf3b4c8115ea25321fdab1fcad998b
mrtska <mrtska.starring@gmail.com>
2016-02-22 01:45:28 +0900

    アップデートチェック処理を修正

--- SRNicoNico/Models/NicoNicoViewer/UpdateCheck.cs
+++ SRNicoNico/Models/NicoNicoViewer/UpdateCheck.cs
@@ -8,19 +8,23 @@ using Livet;
 using Codeplex.Data;
 
 using SRNicoNico.Models.NicoNicoWrapper;
+using System.Xml;
 
 namespace SRNicoNico.Models.NicoNicoViewer {
     public class UpdateCheck : NotificationObject {
 
-        private static string CheckUrl = "https://mrtska.net/niconicowrapper/update";
+        private static string CheckUrl = "http://download.mrtska.net/DownloadCounter/Download?file=NicoNicoViewer/updatea";
         
         public static bool IsUpdateAvailable(double cur, ref string url) {
-
 
             try {
 
                 var a = NicoNicoWrapperMain.Session.GetAsync(CheckUrl).Result;
 
+                if(a.Length == 0) {
+
+                    return false;
+                }
                 var json = DynamicJson.Parse(a);
 
                 if(cur < json.version) {
@@ -31,7 +35,7 @@ namespace SRNicoNico.Models.NicoNicoViewer {
 
 
                 return false;
-            } catch(RequestTimeout) {
+            } catch(Exception ex) when (ex is XmlException || ex is RequestTimeout) {
 
                 return false;
             }
