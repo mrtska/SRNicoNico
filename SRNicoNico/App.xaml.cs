3c7fddf5c5772bb3d741c5ff5fa3e3cf2fa588b7
mrtska <mrtska.starring@gmail.com>
2016-08-19 12:45:08 +0900

    Add comment.

--- SRNicoNico/App.xaml.cs
+++ SRNicoNico/App.xaml.cs
@@ -20,7 +20,7 @@ namespace SRNicoNico {
 	/// </summary>
 	public partial class App : Application {
 
-
+		//メインウィンドウのViewModel
 		public static MainWindowViewModel ViewModelRoot { get; private set; }
 
 
@@ -32,9 +32,12 @@ namespace SRNicoNico {
 		protected override void OnStartup(StartupEventArgs e) {
 			base.OnStartup(e);
 
+		//UIスレッドのディスパッチャを登録しておく
             DispatcherHelper.UIDispatcher = Dispatcher;
 			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
             
+            //WebBrowserコントロールのIEバージョンを最新にする 古いとUI崩れるからね
+            //レジストリを弄るのはここだけ
             Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", "SRNicoNico.exe", 0x00002AF9, Microsoft.Win32.RegistryValueKind.DWord);
             
 
@@ -46,7 +49,7 @@ namespace SRNicoNico {
         //集約エラーハンドラ
         private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
 		{
-		    //TODO:ロギング処理など
+		    //ハンドルされない例外の処理 このメソッドが終わると例外を吐いた
 		    MessageBox.Show(
 		        "不明なエラーが発生しました。可能であれば、この文章をコピーして作者に報告していただれば幸いです。Ctrl+Cでコピーできます。\n ExceptionObject:" + e.ExceptionObject,
 		        "エラー",
