be3a8231d5a3981365e5fcdbca8ae2a4c9f383ba
StarRing <mrtska.starring@gmail.com>
2017-03-09 22:06:41 +0900

    release 1.05 fixed #63, fixed #69, closed #70, fixed #71

--- SRNicoNico/ViewModels/MainWindowViewModel.cs
+++ SRNicoNico/ViewModels/MainWindowViewModel.cs
@@ -22,7 +22,7 @@ namespace SRNicoNico.ViewModels {
         //現在のバージョン
         public double CurrentVersion {
 
-            get { return 1.04; }
+            get { return 1.05; }
         }
 
         #region Title変更通知プロパティ
