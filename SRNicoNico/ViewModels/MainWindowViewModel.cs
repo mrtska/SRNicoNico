55ce9455e23a27ef85b09f6c0e2d263926e52ebc
StarRing <mrtska.starring@gmail.com>
2017-06-21 20:21:02 +0900

    release 1.12 fixed #88

--- SRNicoNico/ViewModels/MainWindowViewModel.cs
+++ SRNicoNico/ViewModels/MainWindowViewModel.cs
@@ -22,7 +22,7 @@ namespace SRNicoNico.ViewModels {
         //現在のバージョン
         public double CurrentVersion {
 
-            get { return 1.11; }
+            get { return 1.12; }
         }
 
         #region Title変更通知プロパティ
