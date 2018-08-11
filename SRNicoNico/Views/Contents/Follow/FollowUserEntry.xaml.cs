dd6dc341247e5d78ac3fb4fc85bb5898706b72a1
mrtska <mrtska.starring@gmail.com>
2018-08-12 05:29:05 +0900

    フォロー機能UI調整

--- SRNicoNico/Views/Contents/Follow/FollowUserEntry.xaml.cs
+++ SRNicoNico/Views/Contents/Follow/FollowUserEntry.xaml.cs
@@ -1,23 +1,6 @@
-﻿using SRNicoNico.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace SRNicoNico.Views {
-    /// <summary>
-    /// FollowUserEntry.xaml の相互作用ロジック
-    /// </summary>
     public partial class FollowUserEntry : UserControl {
         public FollowUserEntry() {
             InitializeComponent();
