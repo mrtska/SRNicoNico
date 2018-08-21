04587bbab468cdc4c1762644912c450b85aa014b
mrtska <mrtska.starring@gmail.com>
2018-08-22 01:45:15 +0900

    DisableScrollViewerFlowDocumentScrollViewerを改良

--- SRNicoNico/Views/Controls/DisabledScrollViewerFlowDocumentScrollViewer.cs
+++ SRNicoNico/Views/Controls/DisabledScrollViewerFlowDocumentScrollViewer.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 
 namespace SRNicoNico.Views.Controls {
@@ -6,9 +8,24 @@ namespace SRNicoNico.Views.Controls {
 
         protected override void OnMouseWheel(MouseWheelEventArgs e) {
 
+            var host = GetType().GetProperty("ScrollViewer", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var scroll = host.GetValue(this) as ScrollViewer;
+            var info = scroll.GetType().GetProperty("ScrollInfo", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var scrollInfo = info.GetValue(scroll) as IScrollInfo;
+
+            if (e.Delta < 0) {
+
+                scrollInfo.MouseWheelDown();
+            } else {
+
+                scrollInfo.MouseWheelUp();
+            }
             // FlowDocumentScrollViewerのスクロール処理を無効
             // FlowDocumentScrollViewerのScrollViewerは、スクロール処理がこのイベントを受け取った後に
             // _ContentHost.LineDown()/LineUp()しか呼んでいなく、スクロール量を全く考慮しないので使いづらい
+            // リフレクションでScrollInfoを取得してマウスホイール用のスクロールメソッドを呼び出す
         }
     }
 }
