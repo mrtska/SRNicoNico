f3f91bc54f3aa915fc0e33e3b468cc7c92ab243c
mrtska <mrtska.starring@gmail.com>
2018-10-02 04:29:57 +0900

    不要な設定・ファイルを削除

--- SRNicoNico/Views/Contents/Settings/SettingsVideoInfoPlacement.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Controls;
-
-namespace SRNicoNico.Views {
-    public partial class SettingsVideoInfoPlacement : UserControl {
-        public SettingsVideoInfoPlacement() {
-            InitializeComponent();
-        }
-    }
-}
