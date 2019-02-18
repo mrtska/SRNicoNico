60aec7849001dd9500af89026d50b055c4461b71
mrtska <mrtska.starring@gmail.com>
2019-02-19 01:37:01 +0900

    release 1.40

--- SRNicoNico/ViewModels/MainWindowViewModel.cs
+++ SRNicoNico/ViewModels/MainWindowViewModel.cs
@@ -14,7 +14,7 @@ namespace SRNicoNico.ViewModels {
         //現在のバージョン
         public double CurrentVersion {
 
-            get { return 1.391; }
+            get { return 1.40; }
         }
 
         #region Title変更通知プロパティ
