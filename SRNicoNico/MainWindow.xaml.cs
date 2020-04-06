f901415d5a6dd7938f863534ead2178a20f9d5d7
mrtska <mrtska.starring@gmail.com>
2020-04-07 01:45:09 +0900

    .NET Core3.1プロジェクト作成

---
+++ SRNicoNico/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace SRNicoNico {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
