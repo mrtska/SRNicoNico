7566078a57f0553679ddf68b4f860214e84c4bd3
mrtska <mrtska.starring@gmail.com>
2020-07-28 02:37:32 +0900

    公開マイリスト仕様変更対応

--- SRNicoNico/Models/NicoNicoWrapper/NicoNicoPublicMylist.cs
+++ SRNicoNico/Models/NicoNicoWrapper/NicoNicoPublicMylist.cs
@@ -4,6 +4,7 @@ using SRNicoNico.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -12,14 +13,14 @@ using System.Web;
 namespace SRNicoNico.Models.NicoNicoWrapper {
     public class NicoNicoPublicMylist {
 
-        private string MylistUrl;
+        private string MylistId;
 
         private TabItemViewModel Owner;
 
-        public NicoNicoPublicMylist(TabItemViewModel vm, string mylistUrl) {
+        public NicoNicoPublicMylist(TabItemViewModel vm, string mylistId) {
 
             Owner = vm;
-            MylistUrl = mylistUrl;
+            MylistId = Regex.Match(mylistId, @"(\d+)$").Groups[1].Value;
         }
 
 
@@ -27,211 +28,69 @@ namespace SRNicoNico.Models.NicoNicoWrapper {
 
             try {
 
+                var query = new GetRequestQuery($"https://nvapi.nicovideo.jp/v2/mylists/{MylistId}");
+                query.AddQuery("page", 1);
+                query.AddQuery("pageSize", 200);
 
-                var a = await App.ViewModelRoot.CurrentUser.Session.GetAsync(MylistUrl);
+                var request = new HttpRequestMessage(HttpMethod.Get, query.TargetUrl);
+                request.Headers.Add("X-Frontend-Id", "6");
+                request.Headers.Add("X-Frontend-Version", "0");
+                request.Headers.Add("X-Niconico-Language", "ja-jp");
 
-                var index = a.IndexOf("Jarty.globals(");
+                var a = await App.ViewModelRoot.CurrentUser.Session.GetAsync(request);
 
-                if(index == -1) {
+                var json = DynamicJson.Parse(a);
+                if (json.meta.status != 200) {
 
                     return null;
                 }
 
-                //該当JavaScriptの部分から取得
-                var globals = a.Substring(index);
-
-                //改行で分割
-                var splitted = globals.Split('\n');
-
-                //正規表現でダブルクォーテ内の名前を取得
-                var regex = new Regex("\"(.*)\"");
-
-                string nickname = null;
-                string userid = null;
-                string mylistname = null;
-                string description = null;
-
-                string json = null;
-
-                foreach(var text in splitted) {
-
-                    //マイリストオーナーだったら
-                    if(text.Contains("mylist_owner:")) {
-
-                        //マイリストオーナーのニックネームを取得
-                        nickname = text.Substring(text.IndexOf("nickname: "));
-
-                        var match = regex.Match(nickname);
-
-                        //グループから取得
-                        nickname = match.Groups[1].Value;
-                        continue;
-                    }
-
-                    if(nickname != null && text.Contains("user_id:")) {
-
-                        userid = new Regex(@"\d+").Match(text).Value;
-                        continue;
-                    }
-
-                    if(userid != null && text.Contains("name:")) {
-
-                        mylistname = regex.Match(text).Groups[1].Value;
-                        continue;
-                    }
-                    if(mylistname != null && text.Contains("description:")) {
-
-                        description = regex.Match(text).Groups[1].Value;
-                        continue;
-                    }
-                    if(description != null && text.Contains("Mylist.preload(")) {
-
-                        //Json取得
-                        json = text.Substring(text.IndexOf(",") + 1, text.Length - text.IndexOf(",") - 3);
-                        break;
-                    }
-                }
-
+                var userId = json.data.mylist.owner.id;
+                var userName = json.data.mylist.owner.name;
+                var mylistName = json.data.mylist.name;
+                var description = json.data.mylist.description;
                 var ret = new NicoNicoPublicMylistGroupEntry();
-
-                ret.OwnerName = @"<a href=""https://www.nicovideo.jp/user/" + userid + @""">" + nickname + "</a> さんの公開マイリスト";
-                ret.Name = mylistname;
+                ret.OwnerName = @"<a href=""https://www.nicovideo.jp/user/" + userId + @""">" + userName + "</a> さんの公開マイリスト";
+                ret.Name = mylistName;
                 ret.Description = description;
-
-                //\nを改行に置換
-                ret.Description = ret.Description.Replace("\\n", "<br>").Replace("\\r", "");
-
                 ret.Description = HyperLinkReplacer.Replace(ret.Description);
 
                 var list = new List<NicoNicoMylistEntry>();
-
-                if(json != null) {
-
-                    StoreItems(json, list);
-                }
-
+                StoreItems(json.data.mylist.items, list);
 
                 ret.Data = list;
 
                 return ret;
-            } catch(RequestFailed) {
+            } catch (RequestFailed) {
 
                 Owner.Status = "公開マイリストの取得に失敗しました";
                 return null;
             }
-
         }
 
-
         //jsonをパースしてリストに入れる
-        private void StoreItems(string str, List<NicoNicoMylistEntry> ret) {
+        private void StoreItems(dynamic json, List<NicoNicoMylistEntry> ret) {
 
-            var json = DynamicJson.Parse(str);
+            foreach (var entry in json) {
 
-            foreach(var entry in json) {
 
-                //普通のマイリストはなぜかdoubleでとりあえずマイリストはstringなんだよね 意味分からん
-                if(entry.item_type is double) {
+                var data = new NicoNicoMylistVideoEntry();
 
-                    entry.item_type = entry.item_type.ToString();
-                }
+                data.ItemId = entry.itemId.ToString();
+                data.Description = entry.description;
+                data.CreateTime = UnixTime.ToUnixTime(DateTime.Parse(entry.addedAt));
 
-                if(entry.item_type is string) {
+                data.VideoId = entry.watchId;
+                data.ContentUrl = "https://www.nicovideo.jp/watch/" + data.VideoId;
+                data.Title = entry.video.title;
+                data.ThumbNailUrl = entry.video.thumbnail.url;
+                data.FirstRetrieve = UnixTime.ToUnixTime(DateTime.Parse(entry.video.registeredAt));
+                data.ViewCount = (int)entry.video.count.view;
+                data.MylistCount = (int)entry.video.count.mylist;
+                data.CommentCount = (int)entry.video.count.comment;
+                data.Length = (int)entry.video.duration;
 
-                    var type = entry.item_type as string;
-                    var item = entry.item_data;
-                    switch(type) {
-                        case "0": {
-
-                                var data = new NicoNicoMylistVideoEntry();
-
-                                data.ItemId = entry.item_id;
-                                data.Description = entry.description;
-                                data.CreateTime = (long)entry.create_time;
-                                data.UpdateTime = (long)entry.update_time;
-
-                                data.VideoId = item.video_id;
-                                data.ContentUrl = "https://www.nicovideo.jp/watch/" + data.VideoId;
-                                data.Title = HttpUtility.HtmlDecode(item.title);
-                                data.ThumbNailUrl = item.thumbnail_url;
-                                data.FirstRetrieve = (long)item.first_retrieve;
-                                data.ViewCount = int.Parse(item.view_counter);
-                                data.MylistCount = int.Parse(item.mylist_counter);
-                                data.CommentCount = int.Parse(item.num_res);
-                                data.Length = int.Parse(item.length_seconds);
-
-                                ret.Add(data);
-                                break;
-                            }
-                        case "5": {
-
-                                var data = new NicoNicoMylistMangaEntry();
-
-                                data.ItemId = entry.item_id;
-                                data.Description = entry.description;
-                                data.CreateTime = (long)entry.create_time;
-                                data.UpdateTime = (long)entry.update_time;
-
-                                data.MangaId = "mg" + item.id;
-                                data.ContentUrl = "http://seiga.nicovideo.jp/watch/" + data.MangaId;
-                                data.Title = HttpUtility.HtmlDecode(item.title);
-                                data.PictureCount = (int)item.res_count;
-                                data.ViewCount = (int)item.view_count;
-                                data.CommentCount = (int)item.comment_count;
-                                data.MylistCount = (int)item.mylist_count;
-                                data.ThumbNailUrl = item.thumbnail_url;
-                                data.FirstRetrieve = (long)item.create_time;
-
-                                ret.Add(data);
-                                break;
-                            }
-                        case "6": {
-
-                                var data = new NicoNicoMylistBookEntry();
-
-                                data.ItemId = entry.item_id;
-                                data.Description = entry.description;
-                                data.CreateTime = (long)entry.create_time;
-                                data.UpdateTime = (long)entry.update_time;
-
-                                data.BookId = "bk" + item.id;
-                                data.ContentUrl = "http://seiga.nicovideo.jp/watch/" + data.BookId;
-                                data.Title = HttpUtility.HtmlDecode(item.title);
-                                data.ThumbNailUrl = item.thumbnail_url;
-                                data.ViewCount = (int)item.view_count;
-                                data.CommentCount = (int)item.comment_count;
-                                data.MylistCount = (int)item.mylist_count;
-                                data.FirstRetrieve = (long)item.released_at;
-
-                                ret.Add(data);
-                                break;
-                            }
-                        case "13": {
-
-                                var data = new NicoNicoMylistArticleEntry();
-
-                                data.ItemId = entry.item_id;
-                                data.Description = entry.description;
-                                data.CreateTime = (long)entry.create_time;
-                                data.UpdateTime = (long)entry.update_time;
-
-                                data.ArticleId = "ar" + item.id;
-                                data.ScreenName = item.screen_name;
-                                data.ContentUrl = "http://ch.nicovideo.jp/" + data.ScreenName + "/blomaga/" + data.ArticleId;
-                                data.Title = HttpUtility.HtmlDecode(item.title);
-                                data.ThumbNailUrl = item.thumbnail_url;
-                                data.CommentCount = (int)item.comment_count;
-                                data.MylistCount = int.Parse(item.mylist_count);
-                                data.FirstRetrieve = (long)item.create_time;
-
-                                ret.Add(data);
-                                break;
-                            }
-                        default:
-                            //な、なんだって～
-                            break;
-                    }
-                }
+                ret.Add(data);
             }
         }
     }
