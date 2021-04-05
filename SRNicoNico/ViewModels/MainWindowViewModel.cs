1fc40f9b7547fafa4ebcfe106f3da1cb02e44f9c
mrtska <mrtska.starring@gmail.com>
2021-04-06 00:12:02 +0900

    release 1.511

--- SRNicoNico/ViewModels/MainWindowViewModel.cs
+++ SRNicoNico/ViewModels/MainWindowViewModel.cs
@@ -14,7 +14,7 @@ namespace SRNicoNico.ViewModels {
         //現在のバージョン
         public double CurrentVersion {
             
-            get { return 1.51; }
+            get { return 1.511; }
         }
 
         #region Title変更通知プロパティ
