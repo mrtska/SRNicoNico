a85d401f5c9daec10794069e4bad79e29804d890
mrtska <mrtska.starring@gmail.com>
2021-05-23 18:59:27 +0900

    Web部分のファイル作成

---
+++ SRNicoNico/Html/webpack.config.ts
@@ -0,0 +1,12 @@
+﻿import * as webpack from 'webpack';
+
+const config: webpack.Configuration = {
+    mode: 'production',
+    entry: [ './Html\\videolayer.ts', './Html\\commentlayer.ts' ],
+    output: {
+        path: __dirname,
+        filename: 'player.bundle.js',
+    },
+};
+
+export default config;
